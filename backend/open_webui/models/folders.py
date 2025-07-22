a77dc40072fba9161deda32b613546e2c42dd248
ANSON-ACER\anson <anson@transnomis.com>
2025-07-22 16:07:23 -0400

    Fix imports

--- backend/open_webui/models/folders.py
+++ backend/open_webui/models/folders.py
@@ -4,7 +4,6 @@ import uuid
 from typing import Optional
 
 from open_webui.internal.db import Base, get_db
-from open_webui.models.chats import Chats
 
 from open_webui.env import SRC_LOG_LEVELS
 from pydantic import BaseModel, ConfigDict
@@ -35,7 +34,7 @@ class Folder(Base):
     created_at = Column(BigInteger)
     updated_at = Column(BigInteger)
     chats = relationship(
-        "Chat",
+        "open_webui.models.chats.Chat",
         back_populates="folder",
         cascade="all, delete-orphan",
     )
